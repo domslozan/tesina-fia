679fa39b07e482bbe84d685802583b7a6a286a51
Riccardo Zanol <ricc@zanol.eu>
2014-12-29 14:24:37 +0100

    Aggiungi un ostacolo non rettangolare

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ import java.awt.Graphics2D;
 import java.awt.Graphics;
 import java.awt.Shape;
 import java.awt.Stroke;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
@@ -96,7 +97,7 @@ public class Main {
         walls.add(new Rectangle2D.Double(0, 295, 195, 10));
         walls.add(new Rectangle2D.Double(245, 295, 50, 10));
 
-        walls.add(new Rectangle2D.Double(350, 150, 100, 100));
+        walls.add(new Ellipse2D.Double(350, 150, 100, 150));
 
         WallListTileMap map = new WallListTileMap(TILE_SIZE, 60, 40, 0, 0, walls);
         TilingGraphBuilder tgb = new TilingGraphBuilder(map);
