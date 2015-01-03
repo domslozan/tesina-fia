b2e1f3df525ba34e36815236c34bcf761f1f089e
Riccardo Zanol <ricc@zanol.eu>
2015-01-03 18:00:18 +0100

    Sistema Main, comincia ad aggiungere la legenda

--- src/Main.java
+++ src/Main.java
@@ -1,97 +1,29 @@
 
-import algorithms.AStar;
-import algorithms.Dijkstra;
-import algorithms.EuclideanDistanceHeuristicFactory;
-import graph.TilingGraphBuilder;
 import java.awt.BasicStroke;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics2D;
 import java.awt.Graphics;
 import java.awt.Shape;
 import java.awt.Stroke;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import javax.swing.*;
-import org.jgrapht.ext.DOTExporter;
-import org.jgrapht.graph.*;
-import org.jgrapht.generate.*;
-import org.jgrapht.*;
-import tiling.Tile;
-import tiling.TileMap;
 import tiling.WallListTileMap;
-
-class MapComponent extends JComponent {
-
-    private WallListTileMap map;
-    private List<Point2D> path;
-    //private BufferedImage image;
-
-    public MapComponent(WallListTileMap map) {
-        this.map = map;
-        //image = new BufferedImage((int) wm.getBorder().getWidth(), (int) wm.getBorder().getHeight(), BufferedImage.TYPE_INT_ARGB);
-        setPreferredSize(new Dimension((int) map.getWidth(), (int) map.getHeight()));
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        Graphics2D g2d = (Graphics2D) g;
-        drawTiles(g2d);
-        drawWalls(g2d);
-        drawPath(g2d);
-    }
-
-    private void drawWalls(Graphics2D g) {
-        Iterator<Shape> i = map.wallIterator();
-        g.setColor(Color.BLACK);
-        while (i.hasNext()) {
-            Shape s = i.next();
-            g.fill(s);
-        }
-    }
-
-    private void drawTiles(Graphics2D g) {
-        Iterator<Tile> i = map.iterator();
-        g.setColor(Color.GRAY);
-        //g.setStroke(new BasicStroke(0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1f, new float[]{5f, 5f}, 0f));
-        while (i.hasNext()) {
-            g.draw(i.next().asRectangle());
-        }
-    }
-
-    private void drawPath(Graphics2D g) {
-        if (path == null) {
-            return;
-        }
-        g.setColor(Color.RED);
-        Iterator<Point2D> i = path.iterator();
-        Point2D last = i.next();
-        while (i.hasNext()) {
-            Point2D n = i.next();
-            g.drawLine((int) last.getX(), (int) last.getY(), (int) n.getX(), (int) n.getY());
-            last = n;
-        }
-    }
-
-    public void setPath(List<Point2D> path) {
-        this.path = path;
-    }
-}
+import ui.MapPanel;
 
 public class Main {
 
-    public static final double TILE_SIZE = 10;
+    private Main() {
+    }
+    private static final double TILE_SIZE = 10;
+    private static WallListTileMap map;
 
-    public static void main(String[] argv) {
+    private static List<Shape> walls() {
         List<Shape> walls = new ArrayList<Shape>();
         walls.add(new Rectangle2D.Double(295, 0, 10, 305));
         walls.add(new Rectangle2D.Double(0, 295, 195, 10));
@@ -99,68 +31,82 @@ public class Main {
 
         walls.add(new Ellipse2D.Double(350, 150, 100, 150));
 
-	walls.add(new Rectangle2D.Double(350, 90, 160, 10));
-	walls.add(new Rectangle2D.Double(500, 120, 10, 260));
+        walls.add(new Rectangle2D.Double(350, 90, 160, 10));
+        walls.add(new Rectangle2D.Double(500, 120, 10, 260));
+        return walls;
+    }
 
-        WallListTileMap map = new WallListTileMap(TILE_SIZE, 60, 40, 0, 0, walls);
-        TilingGraphBuilder tgb = new TilingGraphBuilder(map);
+    public static void main(String[] argv) {
+        map = new WallListTileMap(TILE_SIZE, 60, 40, 0, 0, walls());
+        SwingUtilities.invokeLater(new Runnable() {
 
-        Graph<Tile, DefaultWeightedEdge> tileGraph = tgb.buildGraph();
+            @Override
+            public void run() {
+                buildUI();
+            }
+        });
 
-        try {
-            DOTExporter de = new DOTExporter();
-            FileWriter out = new FileWriter("tilegraph.dot");
-            de.export(out, tileGraph);
-            out.close();
-        } catch (IOException e) {
-            throw new RuntimeException();
-        }
+    }
 
-        Tile start = map.getTileContaining(new Point2D.Double(5, 5));
-        Tile goal = map.getTileContaining(new Point2D.Double(595, 5));
-
-        AStar<Tile, DefaultWeightedEdge> astar = new AStar<Tile, DefaultWeightedEdge>(new EuclideanDistanceHeuristicFactory());
-        Dijkstra<Tile, DefaultWeightedEdge> dijkstra = new Dijkstra<Tile, DefaultWeightedEdge>();
-        List<Tile> vertices = dijkstra.getPath(tileGraph, start, goal);
-
-        ArrayList<Point2D> pointPath = new ArrayList<Point2D>();
-        for (Tile t : vertices) {
-            pointPath.add(t.getCenter());
-        }
-
-        MapComponent mc = new MapComponent(map);
-        mc.setPath(pointPath);
-
-        JFrame frame = new JFrame("Demo");
+    private static void buildUI() {
+        JFrame frame = new JFrame("Pathfinding demo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(mc);
+
+        MapPanel mapPanel = new MapPanel(Main.map);
+        frame.getContentPane().add(mapPanel, BorderLayout.CENTER);
+
+        JPanel top = new JPanel();
+        top.add(new MapLegend());
+        frame.add(top, BorderLayout.NORTH);
+
         frame.pack();
         frame.setVisible(true);
-
-        //random_graph();
     }
-//    public static void random_graph() {
-//	RandomGraphGenerator<Integer, DefaultEdge> rgg = new RandomGraphGenerator<Integer, DefaultEdge>(100, 300);
-//	DefaultDirectedGraph<Integer, DefaultEdge> graph = new DefaultDirectedGraph<Integer, DefaultEdge>(DefaultEdge.class);
-//
-//	VertexFactory<Integer> fac = new VertexFactory<Integer>() {
-//	    private int n = 0;
-//
-//	    public Integer createVertex() {
-//		return new Integer(n++);
-//	    }
-//	};
-//
-//	rgg.generateGraph(graph, fac, null);
-//
-//	try {
-//	    DOTExporter de = new DOTExporter();
-//	    FileWriter out = new FileWriter("randomgraph.dot");
-//	    de.export(out, graph);
-//	    out.close();
-//	}
-//	catch (IOException e) {
-//	    throw new RuntimeException();
-//	}
-//    }
+}
+
+class MapLegend extends JPanel {
+
+    public MapLegend() {
+        super();
+        this.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
+
+        add(new LegendElement("Theta*", Color.RED, new BasicStroke(4f)));
+        add(new LegendElement("A* with Post Smoothing", Color.BLACK, new BasicStroke(4f)));
+    }
+}
+
+class LegendElement extends JPanel {
+
+    private final String text;
+    private final Color color;
+    private final Stroke stroke;
+    private final JLabel label;
+    private final JPanel line;
+
+    public LegendElement(String text, Color color, Stroke stroke) {
+        super();
+        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        this.color = color;
+        this.text = text;
+        this.stroke = stroke;
+
+        //this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        this.line = new JPanel() {
+
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setStroke(LegendElement.this.stroke);
+                g2d.setColor(LegendElement.this.color);
+                g2d.drawLine(0, this.getHeight() / 2, this.getWidth(), this.getHeight() / 2);
+            }
+        };
+        line.setPreferredSize(new Dimension(15, 10));
+        this.label = new JLabel(text);
+        this.add(line);
+        this.add(Box.createHorizontalStrut(5));
+        this.add(label);
+    }
 }
